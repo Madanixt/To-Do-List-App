d89ca878e763137af22cc6e7a64291ae8746cd07
Madanixt <johnmdn1@gmail.com>
2023-05-30 15:55:14 +0100

    first push

--- to-do-list-app/src/Components/TaskForm.js
+++ to-do-list-app/src/Components/TaskForm.js
@@ -1,41 +1,52 @@
 import React, { useState } from 'react';
 
-const TaskForm = ({ addTask }) => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (title.trim() !== '') {
+const TaskForm = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+  
+    // Form submission handler
+    const handleSubmit = (e) => {
+      e.preventDefault();
+  
       // Create a new task object
       const newTask = {
-        id: Date.now(), // Generate a unique id
+        id: Math.random().toString(36).substr(2, 9), // Generate a random id
         title,
         description,
         done: false,
       };
-      addTask(newTask); // Pass the new task to the parent component
+  
+      // Add the new task to the task list (handle this in your main component)
+      // Example: pass the new task object to a function prop called onTaskCreate
+      onTaskCreate(newTask);
+  
+      // Reset the form inputs
       setTitle('');
       setDescription('');
-    }
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <h2>Create a New Task</h2>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <label htmlFor="description">Description:</label>
+        <textarea
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        ></textarea>
+        <button type="submit">Add Task</button>
+      </form>
+    );
   };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <textarea
-        placeholder="Description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      ></textarea>
-      <button type="submit">Add Task</button>
-    </form>
-  );
-};
-
-export default TaskForm;
+  
+  export default TaskForm;
+  
