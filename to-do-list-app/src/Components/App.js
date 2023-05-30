7241bbfe39c61adeb0ef53f09775f69fe81949b9
Madanixt <johnmdn1@gmail.com>
2023-05-30 15:48:58 +0100

    taskform done

--- to-do-list-app/src/Components/App.js
+++ to-do-list-app/src/Components/App.js
@@ -1,10 +1,20 @@
+import React, { useState } from 'react';
+import TaskForm from './TaskForm';
+import TaskList from './TaskList';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div>
-
+      <TaskForm addTask={addTask} />
+      <TaskList tasks={tasks} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ to-do-list-app/src/Components/TaskForm.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const TaskForm = ({ addTask }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim() !== '') {
+      // Create a new task object
+      const newTask = {
+        id: Date.now(), // Generate a unique id
+        title,
+        description,
+        done: false,
+      };
+      addTask(newTask); // Pass the new task to the parent component
+      setTitle('');
+      setDescription('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></textarea>
+      <button type="submit">Add Task</button>
+    </form>
+  );
+};
+
+export default TaskForm;
